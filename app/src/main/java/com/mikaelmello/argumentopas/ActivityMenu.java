ddcac57432c87000625e050cc6acb89a5356f9c1
MikaelMello <mikaelmello@hotmail.com>
2016-05-17 16:50:06 -0300

    Finished To-Dos from ActivityMenu. Small improvements on the code.

--- app/src/main/java/com/mikaelmello/argumentopas/ActivityMenu.java
+++ app/src/main/java/com/mikaelmello/argumentopas/ActivityMenu.java
@@ -19,6 +19,12 @@ public class ActivityMenu extends Activity {
     // The next activity
     public Intent intent;
 
+    private Spinner spinnerCampus;
+    private Spinner spinnerCotas;
+    private Spinner spinnerL1;
+    private Spinner spinnerL2;
+    private Spinner spinnerL3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,63 +45,43 @@ public class ActivityMenu extends Activity {
                         // Getting the selected items in each spinner
                         // Also replacing commas for dots in EditTexts to be able to convert them
                         // to floats/doubles.
-                        // TODO: Use spinners already initialized in initiateSpinners()
-                        // TODO: or initialize them outside the method and use them as arguments,
-                        // TODO: all of this in order to avoid initializing them twice.
 
-                        // First grade language, brute score and essay score.
-                        Spinner idiomaUm = (Spinner)findViewById(R.id.spinnerLinguaUm);
-                        String idioma1 = idiomaUm.getSelectedItem().toString();
+                        // Getting selected items from spinners as Strings.
+                        String idioma1 = spinnerL1.getSelectedItem().toString();
+                        String idioma2 = spinnerL2.getSelectedItem().toString();
+                        String idioma3 = spinnerL3.getSelectedItem().toString();
+                        String cotas = spinnerCotas.getSelectedItem().toString();
+                        String campusStr = spinnerCampus.getSelectedItem().toString();
 
+                        // Getting written scores as strings and removing commas if there are any.
                         EditText ebUm = (EditText)findViewById(R.id.ebUm);
-                        String escBruto1 = (ebUm.getText().toString()).replace(',', '.');
-
                         EditText redaçãoUm = (EditText)findViewById(R.id.redaçãoUm);
+
+                        String escBruto1 = (ebUm.getText().toString()).replace(',', '.');
                         String redação1 = (redaçãoUm.getText().toString()).replace(',', '.');
 
-                        // Second grade language, brute score and essay score.
-                        Spinner idiomaDois = (Spinner)findViewById(R.id.spinnerLinguaDois);
-                        String idioma2 = idiomaDois.getSelectedItem().toString();
-
                         EditText ebDois = (EditText)findViewById(R.id.ebDois);
-                        String escBruto2 = (ebDois.getText().toString()).replace(',', '.');
-
                         EditText redaçãoDois = (EditText)findViewById(R.id.redaçaoDois);
+
+                        String escBruto2 = (ebDois.getText().toString()).replace(',', '.');
                         String redação2 = (redaçãoDois.getText().toString()).replace(',', '.');
 
-                        // Third grade language
-                        Spinner idiomaTrês = (Spinner)findViewById(R.id.spinnerLinguaTrês);
-                        String idioma3 = idiomaTrês.getSelectedItem().toString();
-
-                        // Quota system (different quotas have different arguments)
-                        Spinner sistemaCotas = (Spinner)findViewById(R.id.spinnerSistema);
-                        String cotas = sistemaCotas.getSelectedItem().toString();
-
-                        // Campus of the chosen course.
-                        Spinner campus = (Spinner)findViewById(R.id.spinnerCampus);
-                        String campusStr = campus.getSelectedItem().toString();
-
-                        // Array storing the number of commas or dots in each numeric field.
-                        // TODO: Remove counting of commas. Reason: they have been replaced above.
-                        int test[] = {count(escBruto1, ','),
-                                count(escBruto1, '.'),
-                                count(escBruto2, ','),
+                        // Array storing the number of dots in each numeric field to check later.
+                        int test[] = {count(escBruto1, '.'),
                                 count(escBruto2, '.'),
-                                count(redação1, ','),
                                 count(redação1, '.'),
-                                count(redação2, ','),
                                 count(redação2, '.')};
 
-                        // Testing of multiple conditions that may cause the app to crash or not
-                        // to work properly. They are: empty fields or invalid numeric input.
+                        // Testing of multiple conditions that may cause the app to crash or to
+                        // not work properly. They are: empty fields or invalid numeric input.
                         if(isEmpty(ebUm)) Toast.makeText(getApplicationContext(),"Preencha o campo de\nEscore Bruto do PAS 1.", Toast.LENGTH_LONG).show();
                         else if(isEmpty(ebDois)) Toast.makeText(getApplicationContext(),"Preencha o campo de\nEscore Bruto do PAS 2.", Toast.LENGTH_LONG).show();
                         else if(isEmpty(redaçãoUm)) Toast.makeText(getApplicationContext(),"Preencha o campo de\nRedação do PAS 1.", Toast.LENGTH_LONG).show();
                         else if(isEmpty(redaçãoDois)) Toast.makeText(getApplicationContext(),"Preencha o campo de\nRedação do PAS 2.", Toast.LENGTH_LONG).show();
-                        else if(test[0] > 1 || test[1] > 1 || test[0] + test[1] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo\n de EB do PAS 1.", Toast.LENGTH_LONG).show();
-                        else if(test[2] > 1 || test[3] > 1 || test[2] + test[3] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo\n de EB do PAS 2.", Toast.LENGTH_LONG).show();
-                        else if(test[4] > 1 || test[5] > 1 || test[4] + test[5] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo de\nRedação do PAS 1.", Toast.LENGTH_LONG).show();
-                        else if(test[6] > 1 || test[7] > 1 || test[6] + test[7] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo de\nRedação do PAS 2.", Toast.LENGTH_LONG).show();
+                        else if(test[0] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo\n de EB do PAS 1.", Toast.LENGTH_LONG).show();
+                        else if(test[1] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo\n de EB do PAS 2.", Toast.LENGTH_LONG).show();
+                        else if(test[2] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo de\nRedação do PAS 1.", Toast.LENGTH_LONG).show();
+                        else if(test[3] > 1) Toast.makeText(getApplicationContext(),"Formato inválido no campo de\nRedação do PAS 2.", Toast.LENGTH_LONG).show();
                         else if(Float.parseFloat(escBruto1) > 85) Toast.makeText(getApplicationContext(),"Valor de Escore Bruto\ndo PAS 1 acima do limite.", Toast.LENGTH_LONG).show();
                         else if(Float.parseFloat(escBruto2) >= 100) Toast.makeText(getApplicationContext(),"Valor de Escore Bruto\ndo PAS 2 acima do limite.", Toast.LENGTH_LONG).show();
                         else if(Float.parseFloat(redação1) > 10) Toast.makeText(getApplicationContext(),"Valor de Redação do PAS 1\nacima do limite.", Toast.LENGTH_LONG).show();
@@ -132,7 +118,7 @@ public class ActivityMenu extends Activity {
     private void initiateSpinners() {
 
         // Initializing spinner of all available Campuses
-        Spinner spinnerCampus = (Spinner)findViewById(R.id.spinnerCampus);
+        spinnerCampus = (Spinner)findViewById(R.id.spinnerCampus);
         final String[] allCampuses = {"Darcy Ribeiro", "Planaltina", "Ceilândia", "Gama"};
 
         ArrayAdapter<String> adapterCampus = new ArrayAdapter<String>(ActivityMenu.this,
@@ -141,7 +127,7 @@ public class ActivityMenu extends Activity {
         spinnerCampus.setAdapter(adapterCampus);
 
         // Spinner of different quotas.
-        Spinner spinnerSistema = (Spinner) findViewById(R.id.spinnerSistema);
+        spinnerCotas = (Spinner) findViewById(R.id.spinnerSistema);
         final String[] allSistemas = {
                 "Nenhum",
                 "Negros",
@@ -153,24 +139,24 @@ public class ActivityMenu extends Activity {
         ArrayAdapter<String> adapterCotas = new ArrayAdapter<String>(ActivityMenu.this,
                 R.layout.spinner_item,allSistemas);
         adapterCotas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerSistema.setAdapter(adapterCotas);
+        spinnerCotas.setAdapter(adapterCotas);
 
         // The three available idioms (Spanish, English and French)
         final String[] idiomasPAS = {"Espanhol", "Inglês", "Francês"};
 
         // Initializing the three variables with their respective spinners.
-        Spinner spinnerLinguaUm = (Spinner)findViewById(R.id.spinnerLinguaUm);
-        Spinner spinnerLinguaDois = (Spinner)findViewById(R.id.spinnerLinguaDois);
-        Spinner spinnerLinguaTrês = (Spinner)findViewById(R.id.spinnerLinguaTrês);
+        spinnerL1 = (Spinner)findViewById(R.id.spinnerLinguaUm);
+        spinnerL2 = (Spinner)findViewById(R.id.spinnerLinguaDois);
+        spinnerL3 = (Spinner)findViewById(R.id.spinnerLinguaTrês);
 
         ArrayAdapter<String> adapterLinguas = new ArrayAdapter<String>(ActivityMenu.this,
                 R.layout.spinner_item,idiomasPAS);
         adapterLinguas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // Using the same adapter to the three spinners.
-        spinnerLinguaUm.setAdapter(adapterLinguas);
-        spinnerLinguaDois.setAdapter(adapterLinguas);
-        spinnerLinguaTrês.setAdapter(adapterLinguas);
+        spinnerL1.setAdapter(adapterLinguas);
+        spinnerL2.setAdapter(adapterLinguas);
+        spinnerL3.setAdapter(adapterLinguas);
     }
 
     /*
